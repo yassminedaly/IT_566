aa07362ddb69a7eb74280b7a72d2aa26dff16ffd
yasmine <yassminedaly@gmail.com>
2025-11-05 17:01:32 -0400

    Connect Database

--- src/gallery/persistence_layer/mysql_persistence_wrapper.py
+++ src/gallery/persistence_layer/mysql_persistence_wrapper.py
@@ -24,13 +24,14 @@ class MySQLPersistenceWrapper(ApplicationBase):
 			self.DATABASE["connection"]["config"]["database"]
 		self.DB_CONFIG['user'] = self.DATABASE["connection"]["config"]["user"]
 		self.DB_CONFIG['host'] = self.DATABASE["connection"]["config"]["host"]
+		self.DB_CONFIG['password'] = self.DATABASE["connection"]["config"]["password"]
 		self.DB_CONFIG['port'] = self.DATABASE["connection"]["config"]["port"]
 
 		self._logger.log_debug(f'{inspect.currentframe().f_code.co_name}: DB Connection Config Dict: {self.DB_CONFIG}')
 
 		# Database Connection
-		#self._connection_pool = \
-			#self._initialize_database_connection_pool(self.DB_CONFIG)
+		self._connection_pool = \
+			self._initialize_database_connection_pool(self.DB_CONFIG)
 		
 
 		# SQL String Constants
