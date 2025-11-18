b61b392720800b3a30945fa35be5a906a474d399
yasmine <yassminedaly@gmail.com>
2025-11-18 16:27:32 -0400

    Do Peristance Wrapper

--- src/gallery/persistence_layer/mysql_persistence_wrapper.py
+++ src/gallery/persistence_layer/mysql_persistence_wrapper.py
@@ -83,3 +83,75 @@ class MySQLPersistenceWrapper(ApplicationBase):
 		except Exception as e:
 			self._logger.log_error(f'{inspect.currentframe().f_code.co_name}:Problem creating connection pool: {e}')
 			self._logger.log_error(f'{inspect.currentframe().f_code.co_name}:Check DB conf:\n{json.dumps(self.DATABASE)}')
+
+	def _get_connection(self):
+		try:
+			self._logger.log_debug("Getting connection")
+			return self._connection_pool.get_connection()
+		except:
+			self._logger.log_error("Could not get connection")
+			return None
+
+	def _execute_select(self, query, params=None):
+		try:
+			conn = self._get_connection()
+			cursor = conn.cursor()
+			cursor.execute(query, params)
+			rows = cursor.fetchall()
+			cursor.close()
+			conn.close()
+			return rows
+		except:
+			self._logger.log_error("Select failed")
+			return []
+
+	def _execute_modify(self, query, params=None):
+		try:
+			conn = self._get_connection()
+			cursor = conn.cursor()
+			cursor.execute(query, params)
+			conn.commit()
+			count = cursor.rowcount
+			cursor.close()
+			conn.close()
+			return count
+		except:
+			self._logger.log_error("Modify failed")
+			return 0
+
+	def get_all_photographers(self):
+		return self._execute_select(self.GET_ALL_PHOTOGRAPHERS)
+
+	def get_all_albums(self):
+		return self._execute_select(self.GET_ALL_ALBUMS)
+
+	def get_all_photos(self):
+		return self._execute_select(self.GET_ALL_PHOTOS)
+
+	def add_photographer(self, photographer_name, email):
+		return self._execute_modify(self.ADD_PHOTOGRAPHER, (photographer_name, email))
+
+	def add_album(self, album_name, creation_date):
+		return self._execute_modify(self.ADD_ALBUM, (album_name, creation_date))
+
+	def add_photo(self, photo_date, id_photographer, id_album):
+		return self._execute_modify(self.ADD_PHOTO, (photo_date, id_photographer, id_album))
+
+	def delete_photographer(self, id_photographer):
+		return self._execute_modify(self.DELETE_PHOTOGRAPHER, (id_photographer,))
+
+	def delete_album(self, id_album):
+		return self._execute_modify(self.DELETE_ALBUM, (id_album,))
+
+	def delete_photo(self, id_photo):
+		return self._execute_modify(self.DELETE_PHOTO, (id_photo,))
+
+	def change_photographer_email(self, id_photographer, new_email):
+		return self._execute_modify(self.CHANGE_PHOTOGRAPHER_EMAIL, (new_email, id_photographer))
+
+	def change_album_name(self, id_album, new_name):
+		return self._execute_modify(self.CHANGE_ALBUM_NAME, (new_name, id_album))
+
+	def change_photo_album(self, id_photo, new_album_id):
+		return self._execute_modify(self.CHANGE_PHOTO_ALBUM, (new_album_id, id_photo))
+
